3ec3dedcadc7bc94dc31bfeca433d9b995820be5
Anna von Oesen <155978924+avoesen@users.noreply.github.com>
2024-03-05 16:00:07 +0100

    changed add button

--- components/Styled/IconButtonLarge.js
+++ components/Styled/IconButtonLarge.js
@@ -22,7 +22,7 @@ const StyledBox = styled.div`
   background-color: white;
   position: fixed;
   z-index: 300;
-  bottom: 3rem;
+  bottom: 6rem;
   left: 50%;
   transform: translateX(-50%);
   width: 60px;
