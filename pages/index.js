3bae20e7491cf13d793c53ae17b2388ca8eec730
Björn Jentschke <jentschke1@gmail.com>
2024-03-12 21:39:31 +0100

    add UI filter function

--- pages/index.js
+++ pages/index.js
@@ -23,9 +23,21 @@ export default function HomePage({
   const router = useRouter();
 
   const [isFilterButton, setIsFilterButton] = useState(false);
-  const [isDifficultyButton, setIsDifficultyButton] = useState(false);
-  const [isDurationButton, setIsDurationButton] = useState(false);
-  const [isFoodPreferencesButton, setIsFoodPreferencesButton] = useState(false);
+  const [isDifficultyButton, setIsDifficultyButton] = useState([
+    false,
+    false,
+    false,
+  ]);
+  const [isDurationButton, setIsDurationButton] = useState([
+    false,
+    false,
+    false,
+  ]);
+  const [isNutritionButton, setIsNutritionButton] = useState([
+    false,
+    false,
+    false,
+  ]);
 
   if (recipesError || error) {
     return (
@@ -52,18 +64,30 @@ export default function HomePage({
 
   function toggleFilter() {
     setIsFilterButton(!isFilterButton);
-    setIsDifficultyButton(false);
-    setIsDurationButton(false);
-    setIsFoodPreferencesButton(false);
   }
-  function toggleDifficulty() {
-    setIsDifficultyButton(!isDifficultyButton);
+
+  function toggleDifficulty(index) {
+    const updatedState = [...isDifficultyButton];
+    updatedState[index] = !updatedState[index];
+    setIsDifficultyButton(updatedState);
   }
-  function toggleDuration() {
-    setIsDurationButton(!isDurationButton);
+
+  function toggleDuration(index) {
+    const updatedState = [...isDurationButton];
+    updatedState[index] = !updatedState[index];
+    setIsDurationButton(updatedState);
   }
-  function toggleFoodPreferences() {
-    setIsFoodPreferencesButton(!isFoodPreferencesButton);
+
+  function toggleNutrition(index) {
+    const updatedState = [...isNutritionButton];
+    updatedState[index] = !updatedState[index];
+    setIsNutritionButton(updatedState);
+  }
+
+  function resetCategories() {
+    setIsDifficultyButton([false, false, false]);
+    setIsDurationButton([false, false, false]);
+    setIsNutritionButton([false, false, false, false, false]);
   }
 
   return (
@@ -75,46 +99,96 @@ export default function HomePage({
           id="Layer_1"
           data-name="Layer 1"
           viewBox="0 0 24 24"
-          width="30"
-          height="30"
+          width="20"
+          height="20"
         >
           <path d="m4,4.036V.5c0-.276-.224-.5-.5-.5s-.5.224-.5.5v3.536c-1.694.243-3,1.704-3,3.464s1.306,3.221,3,3.464v12.536c0,.276.224.5.5.5s.5-.224.5-.5v-12.536c1.694-.243,3-1.704,3-3.464s-1.306-3.221-3-3.464Zm-.5,5.964c-1.379,0-2.5-1.121-2.5-2.5s1.121-2.5,2.5-2.5,2.5,1.121,2.5,2.5-1.121,2.5-2.5,2.5Zm9,3.036V.5c0-.276-.224-.5-.5-.5s-.5.224-.5.5v12.536c-1.694.243-3,1.704-3,3.464s1.306,3.221,3,3.464v3.536c0,.276.224.5.5.5s.5-.224.5-.5v-3.536c1.694-.243,3-1.704,3-3.464s-1.306-3.221-3-3.464Zm-.5,5.964c-1.379,0-2.5-1.121-2.5-2.5s1.121-2.5,2.5-2.5,2.5,1.121,2.5,2.5-1.121,2.5-2.5,2.5Zm12-11.5c0-1.76-1.306-3.221-3-3.464V.5c0-.276-.224-.5-.5-.5s-.5.224-.5.5v3.536c-1.694.243-3,1.704-3,3.464s1.306,3.221,3,3.464v12.536c0,.276.224.5.5.5s.5-.224.5-.5v-12.536c1.694-.243,3-1.704,3-3.464Zm-3.5,2.5c-1.379,0-2.5-1.121-2.5-2.5s1.121-2.5,2.5-2.5,2.5,1.121,2.5,2.5-1.121,2.5-2.5,2.5Z" />
         </svg>
       </StyledFilterButton>
       {isFilterButton && (
-        <StyledTopDiv>
-          <StyledTopButton onClick={toggleDifficulty}>
-            difficulty
-          </StyledTopButton>
-          <StyledTopButton onClick={toggleDuration}>duration</StyledTopButton>
-          <StyledTopButton onClick={toggleFoodPreferences}>
-            food preferences
-          </StyledTopButton>
-        </StyledTopDiv>
+        <>
+          <StyledH2>Schwierigkeit</StyledH2>
+          <StyledResetButton onClick={resetCategories}>
+            alles Zurücksetzen
+          </StyledResetButton>
+          <StyledCategoriesDiv>
+            <StyledCategoryButton
+              onClick={() => toggleDifficulty(0)}
+              isActive={isDifficultyButton[0]}
+            >
+              einfach
+            </StyledCategoryButton>
+            <StyledCategoryButton
+              onClick={() => toggleDifficulty(1)}
+              isActive={isDifficultyButton[1]}
+            >
+              mittel
+            </StyledCategoryButton>
+            <StyledCategoryButton
+              onClick={() => toggleDifficulty(2)}
+              isActive={isDifficultyButton[2]}
+            >
+              hart
+            </StyledCategoryButton>
+          </StyledCategoriesDiv>
+          <StyledH2>Zubereitungsdauer</StyledH2>
+          <StyledCategoriesDiv>
+            <StyledCategoryButton
+              onClick={() => toggleDuration(0)}
+              isActive={isDurationButton[0]}
+            >
+              {" "}
+              {"< 10"} Min
+            </StyledCategoryButton>
+            <StyledCategoryButton
+              onClick={() => toggleDuration(1)}
+              isActive={isDurationButton[1]}
+            >
+              10 - 20 Min
+            </StyledCategoryButton>
+            <StyledCategoryButton
+              onClick={() => toggleDuration(2)}
+              isActive={isDurationButton[2]}
+            >
+              {"> 20 Min"}
+            </StyledCategoryButton>
+          </StyledCategoriesDiv>
+          <StyledH2>Ernährung</StyledH2>
+          <StyledCategoriesDiv>
+            <StyledCategoryButton
+              onClick={() => toggleNutrition(0)}
+              isActive={isNutritionButton[0]}
+            >
+              vegan
+            </StyledCategoryButton>
+            <StyledCategoryButton
+              onClick={() => toggleNutrition(1)}
+              isActive={isNutritionButton[1]}
+            >
+              vegetarisch
+            </StyledCategoryButton>
+            <StyledCategoryButton
+              onClick={() => toggleNutrition(2)}
+              isActive={isNutritionButton[2]}
+            >
+              Fleisch
+            </StyledCategoryButton>
+            <StyledCategoryButton
+              onClick={() => toggleNutrition(3)}
+              isActive={isNutritionButton[3]}
+            >
+              pescetarisch
+            </StyledCategoryButton>
+            <StyledCategoryButton
+              onClick={() => toggleNutrition(4)}
+              isActive={isNutritionButton[4]}
+            >
+              Keto
+            </StyledCategoryButton>
+          </StyledCategoriesDiv>
+        </>
       )}
-      {isDifficultyButton && (
-        <StyledSecondLevelDiv>
-          <StyledSecondLevelButton>easy</StyledSecondLevelButton>
-          <StyledSecondLevelButton>medium</StyledSecondLevelButton>
-          <StyledSecondLevelButton>hard</StyledSecondLevelButton>
-        </StyledSecondLevelDiv>
-      )}
-      {isDurationButton && (
-        <StyledSecondLevelDiv>
-          <StyledSecondLevelButton> {"< 10"} Min</StyledSecondLevelButton>
-          <StyledSecondLevelButton>10 - 20 Min</StyledSecondLevelButton>
-          <StyledSecondLevelButton>{"> 20 Min"}</StyledSecondLevelButton>
-        </StyledSecondLevelDiv>
-      )}
-      {isFoodPreferencesButton && (
-        <StyledSecondLevelDiv>
-          <StyledSecondLevelButton>vegan</StyledSecondLevelButton>
-          <StyledSecondLevelButton>vegetarian</StyledSecondLevelButton>
-          <StyledSecondLevelButton>carnivore</StyledSecondLevelButton>
-          <StyledSecondLevelButton>pescetarian</StyledSecondLevelButton>
-          <StyledSecondLevelButton>keto</StyledSecondLevelButton>
-        </StyledSecondLevelDiv>
-      )}
+
       <StyledUl>
         {recipes?.map((recipe) => {
           return (
@@ -141,45 +215,51 @@ const StyledFilterButton = styled.button`
   background: none;
   border: none;
   position: absolute;
-  top: 0.5rem;
+  top: 1rem;
   right: 2rem;
 `;
 
-const StyledTopDiv = styled.div`
-  display: flex;
-  justify-content: space-between;
-  max-width: calc(100% - (2 * var(--gap-out)));
-  margin: auto;
-  margin-top: 0.5rem;
-  margin-bottom: 0.5rem;
+const StyledResetButton = styled.button`
+  background-color: transparent;
+  border: none;
+  position: absolute;
+  top: 4rem;
+  right: 1.5rem;
+  font-weight: bold;
 `;
 
-const StyledSecondLevelDiv = styled.div`
+const StyledH2 = styled.h2`
+  font-size: large;
+  text-align: left;
+  width: calc(100% - (2 * var(--gap-out)));
+  margin-right: var(--gap-out);
+  margin-left: var(--gap-out);
+  margin-top: var(--gap-between);
+  margin-bottom: var(--gap-between);
+  box-sizing: border-box;
+  position: relative;
+`;
+
+const StyledCategoriesDiv = styled.div`
   display: flex;
   justify-content: center;
+  flex-wrap: wrap;
   max-width: calc(100% - (2 * var(--gap-out)));
   margin: auto;
   margin-top: 0.25rem;
-  flex-wrap: wrap;
 `;
 
-const StyledTopButton = styled.button`
-  color: var(--color-darkgrey);
+const StyledCategoryButton = styled.button`
+  background-color: ${(props) =>
+    props.isActive ? "var(--color-darkgrey)" : "var(--color-component)"};
+  color: ${(props) =>
+    props.isActive ? "var(--color-component)" : "var(--color-darkgrey)"};
   border: solid var(--color-darkgrey) 1px;
-  border: none;
   border-radius: var(--border-radius-small);
-  background-color: var(--color-component);
-  width: 5rem;
-`;
-
-const StyledSecondLevelButton = styled.button`
-  background-color: var(--color-darkgrey);
-  border: none;
-  border-radius: var(--border-radius-small);
-  color: var(--color-component);
   width: 6rem;
-  height: 1.5rem;
-  margin-right: 1rem;
-  margin-bottom: 0.25rem;
+  height: 1.75rem;
+  margin-right: 0.5rem;
+  margin-left: 0.5rem;
+  margin-bottom: 0.5rem;
   padding: 0.25rem;
 `;
