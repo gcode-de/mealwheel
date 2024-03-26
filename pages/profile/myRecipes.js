96e057c5ea5347772267692e124a0ac703930be8
Anna von Oesen <155978924+avoesen@users.noreply.github.com>
2024-03-26 16:32:41 +0100

    delete collections from recipes

--- pages/profile/myRecipes.js
+++ pages/profile/myRecipes.js
@@ -4,12 +4,8 @@ import Header from "@/components/Styled/Header";
 import IconButton from "@/components/Styled/IconButton";
 import { useRouter } from "next/router";
 import Spacer from "@/components/Styled/Spacer";
-import BookPlus from "/public/icons/svg/notebook-alt_add.svg";
 import { useState } from "react";
-import NewCollection from "@/components/Forms/NewCollection";
 
-import Link from "next/link";
-import CollectionCard from "@/components/Cards/CollectionCard";
 import StyledH2 from "@/components/Styled/StyledH2";
 
 import useSWR from "swr";
@@ -75,99 +71,28 @@ export default function MyRecipes({
         onClick={() => router.back()}
       />
       <Spacer />
-
-      <StyledH2>
-        <div>Kochbücher</div>
-        <StyledLink href="/profile/collections">alle anzeigen</StyledLink>
-      </StyledH2>
-      <Wrapper>
-        {user.collections.length
-          ? user.collections.map((col, index) => (
-              <CollectionCard key={index} collection={col} />
-            ))
-          : `Du hast noch keine Kochbücher angelegt.`}
-        <StyledCollection onClick={toggleAddCollection}>
-          <BookPlus width={40} height={40} />
-          <StyledParagraph />
-        </StyledCollection>
-        {isModalCollection && (
-          <NewCollection
-            user={user}
-            mutateUser={mutateUser}
-            setModal={toggleAddCollection}
-          />
-        )}
-      </Wrapper>
       <StyledH2>Meine Rezepte</StyledH2>
-      <StyledArticle>
-        <StyledUl>
-          {myRecipes.length
-            ? myRecipes?.map((recipe) => {
-                return (
-                  <MealCard
-                    key={recipe._id}
-                    recipe={recipe}
-                    isFavorite={getRecipeProperty(recipe._id, "isFavorite")}
-                    onToggleIsFavorite={toggleIsFavorite}
-                  ></MealCard>
-                );
-              })
-            : "Du hast noch keine eigenen Rezepte erstellt."}
-        </StyledUl>
-      </StyledArticle>
+
+      <StyledUl>
+        {myRecipes.length
+          ? myRecipes?.map((recipe) => {
+              return (
+                <MealCard
+                  key={recipe._id}
+                  recipe={recipe}
+                  isFavorite={getRecipeProperty(recipe._id, "isFavorite")}
+                  onToggleIsFavorite={toggleIsFavorite}
+                ></MealCard>
+              );
+            })
+          : "Du hast noch keine eigenen Rezepte erstellt."}
+      </StyledUl>
     </>
   );
 }
 
-const StyledArticle = styled.article``;
-
 const StyledUl = styled.ul`
-  padding: 10px;
+  padding: 0;
   max-width: 350px;
   margin: 0 auto;
-`;
-
-const StyledLink = styled(Link)`
-  color: var(--color-darkgrey);
-  text-decoration: none;
-  font-size: medium;
-`;
-const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: calc(2 * var(--gap-between));
-  margin: auto;
-  margin-bottom: 2rem;
-  width: calc(100% - (2 * var(--gap-out)));
-`;
-const StyledCollection = styled.button`
-  text-decoration: none;
-  color: var(--color-font);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: start;
-  fill: var(--color-lightgrey);
-  color: var(--color-lightgrey);
-  border: 1px solid var(--color-lightgrey);
-  border-radius: var(--border-radius-small);
-  background-color: var(--color-component);
-  justify-content: center;
-  cursor: pointer;
-  margin-top: 0;
-  margin-bottom: 0;
-  height: 93px;
-  &:hover {
-    fill: var(--color-highlight);
-    color: var(--color-highlight);
-  }
-`;
-const StyledParagraph = styled.p`
-  text-align: center;
-  max-width: 100%;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  margin: 0;
-  margin-top: var(--gap-between);
-  height: 2.5;
 `;
