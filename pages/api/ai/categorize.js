b489b4e54c5442fce6950fdc8826751da9c09ade
Samuel Gesang <sgesang@mailo.com>
2024-03-26 17:10:55 +0100

    code cleanup

--- pages/api/ai/categorize.js
+++ pages/api/ai/categorize.js
@@ -50,7 +50,6 @@ export default async function handler(req, res) {
       temperature: 0.6,
       max_tokens: 500,
     });
-    console.log("response in API:\n", response.choices[0].message.content);
 
     return res.status(200).json(response.choices[0].message.content);
   } catch (error) {
