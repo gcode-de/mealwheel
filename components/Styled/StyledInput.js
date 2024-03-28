5b04a517901ea16c1a1a4a081e510870da906440
Anna von Oesen <155978924+avoesen@users.noreply.github.com>
2024-03-28 15:21:27 +0100

    refactor shopping

---
+++ components/Styled/StyledInput.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  background-color: var(--color-background);
+  border: none;
+  border-radius: 10px;
+  height: 30px;
+  width: calc(100% - (2 * var(--gap-out)));
+  flex-grow: ${(props) => props.$flexGrow};
+  padding: 0.7rem;
+`;
+export default Input;
---
+++ components/Styled/Wrapper.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: end;
+  width: calc(100% - (2 * var(--gap-out)));
+  margin: auto;
+`;
+export default Wrapper;
