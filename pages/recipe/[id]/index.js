e77abe4c2c1109bd481c2e4a33113e58200bcac0
Samuel Gesang <sgesang@mailo.com>
2024-05-28 09:45:39 +0200

    Code cleanup

--- pages/recipe/[id]/index.js
+++ pages/recipe/[id]/index.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styled from "styled-components";
 import useSWR from "swr";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useRouter } from "next/router";
 import { notifySuccess, notifyError } from "/helpers/toast";
 import { getFilterLabelByValue } from "@/helpers/filterTags";
